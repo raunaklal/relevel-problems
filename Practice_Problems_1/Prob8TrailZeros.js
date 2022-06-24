80355a30be89f72c1d0c761cec15dc2fd26d3c42
Raunak Kumar Lal <raunaklal@gmail.com>
2022-06-25 01:23:30 +0530

    Prob8_updated

--- Practice_Problems_1/Prob8TrailZeros.js
+++ Practice_Problems_1/Prob8TrailZeros.js
@@ -6,5 +6,11 @@ Given an integer n, write a function that returns the count of trailing zeroes i
 function countTrailingZeros(num){
     if(n<0)
         return -1;
-    
+    let ans = 0;
+    for (let i = 5; Math.floor(n/i) >= 1; i*=5)
+        ans += Math.floor(n/i);
+    return ans;
 }
+
+let n = 5;
+console.log(`Count of trailing 0s in ${n} is ${countTrailingZeros(n)}`);
