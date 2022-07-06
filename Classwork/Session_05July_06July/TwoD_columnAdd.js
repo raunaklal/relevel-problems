f15d538049edfe44685c2d60772253599eb01309
Raunak Kumar Lal <raunaklal@gmail.com>
2022-07-06 22:29:37 +0530

    06_07_2022

--- Classwork/Session_05July_06July/TwoD_columnAdd.js
+++ Classwork/Session_05July_06July/TwoD_columnAdd.js
@@ -18,11 +18,16 @@ let array = [
   [2, 3, 4],
   [4, 4, 4],
 ];
-let ans = Array(array[0].length)
+let ans = Array(array[0].length);
 for (let i = 0; i < array[0].length; i++) {
-    ans[i] = 0;
-    for(let j =0 ; j<array.length;j++){
-        ans[i]+=array[j][i];
-    }
+  ans[i] = 0;
+  for (let j = 0; j < array.length; j++) {
+    ans[i] += array[j][i];
+  }
 }
 console.log(ans);
+
+let arraySum = array.reduce((arr1, arr2) =>
+  arr1.map((sum, id) => sum + arr2[id])
+);
+console.log(arraySum);
